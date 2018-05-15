7fbeb13e55477472af7fba54919681f722aa8f47
Martina Vitanova <31864165+TinaVitanova@users.noreply.github.com>
2018-05-15 20:15:42 +0200

    First Commit
    Initial commit with only a base platform runner

---
+++ PlatformerNajbolje/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+	void Update () {
+		transform.position = new Vector3 (player.position.x + 5,0,-10);
+	}
+}
